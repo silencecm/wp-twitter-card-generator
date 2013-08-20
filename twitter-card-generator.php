720c5ee1d9df11f469e39c3e3c3e2d48f9dfe219
Riley MacDonald <riley.macdonald@teamspace.ca>
2013-08-20 16:22:58 -0300

    Removed unused comments.

--- twitter-card-generator.php
+++ twitter-card-generator.php
@@ -28,7 +28,6 @@ if ( ! defined( 'WPINC' ) ) {
 require_once( plugin_dir_path( __FILE__ ) . 'class-twitter-card-generator.php' );
 
 // Register hooks that are fired when the plugin is activated, deactivated, and uninstalled, respectively.
-// TODO: replace Twitter_Card_Generator with the name of the plugin defined in `class-plugin-name.php`
 register_activation_hook( __FILE__, array( 'Twitter_Card_Generator', 'activate' ) );
 register_deactivation_hook( __FILE__, array( 'Twitter_Card_Generator', 'deactivate' ) );
 
